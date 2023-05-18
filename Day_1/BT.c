7f71fbde4a321475b52cdcdf69a423b2c9028f5d
nammesut <hoangnammst777@gmail.com>
2023-05-18 21:08:03 +0700

    Set Pin high

--- Day_1/BT.c
+++ Day_1/BT.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
+uint8_t PORTA = 0b00100000;
+
 void readByte(uint8_t byte)
 {
     uint8_t temp;
@@ -8,14 +10,30 @@ void readByte(uint8_t byte)
     for(int i=0; i<8; i++)
     {
         temp = ((byte<<i) & 0b10000000)?1:0;
-        printf("%d", temp);   
+        printf("%d", temp); 
     }
+    printf("\n");
 }
 
+typedef enum{
+    PIN0, PIN1, PIN2, PIN3, PIN4, PIN5, PIN6, PIN7
+}pins;
+
+
+void pinHigh(pins pin)
+{
+    PORTA = PORTA | (0b10000000 >> pin); 
+}
+
+void pinLow(pins pin)
+{
+    PORTA = PORTA & ~(0b10000000 >> pin); 
+}
 int main()
 {
-    uint8_t test = 55;
-
-    readByte(test);
+    pinHigh(PIN7);
+    readByte(PORTA);
+    pinLow(PIN2);
+    readByte(PORTA);
     return 0;
 }
