1107fc6fcfb618a89cfa876940f94f22c1615225
nammesut <hoangnammst777@gmail.com>
2023-05-28 10:25:36 +0700

    Calculates size of struct and union

--- ON_TAP/Size_of_Struct_Union.c
+++ ON_TAP/Size_of_Struct_Union.c
@@ -22,9 +22,24 @@ struct sizeofStruct3
     uint16_t c[2];
 }str3;
 
+union sizeofUnion1
+{
+    uint8_t a[5];
+    uint8_t b[3];
+}uni1;
+
+union sizeofUnion2
+{
+    uint32_t a;
+    uint8_t b[17];
+    uint64_t c;
+}uni2;
+
 int main()
 {
     printf("%lu \n", sizeof(str1));  
     printf("%lu \n", sizeof(str2));  
-    printf("%lu \n", sizeof(str3));  
+    printf("%lu \n", sizeof(str3));
+    printf("%lu \n", sizeof(uni1)); 
+    printf("%lu \n", sizeof(uni2)); 
 }
