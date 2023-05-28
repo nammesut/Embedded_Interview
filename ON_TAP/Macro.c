8dd6ce52bfc5c976cc371bb38c9cf2eb4c22f8d4
nammesut <hoangnammst777@gmail.com>
2023-05-28 16:25:32 +0700

    New change

--- ON_TAP/Macro.c
+++ ON_TAP/Macro.c
@@ -4,6 +4,14 @@
 #define MAX 10
 #define SUM(a, b) (a+b)
 
+/*Macro function*/
+#define CREATE_FUNC(name, contents)      \
+void name(){                             \
+    printf("%s\n", contents);            \
+}
+
+CREATE_FUNC(test, "This is test\n");
+
 /*Inline*/
 inline int MIN(int a, int b){
     return (a<b)?a:b;
@@ -13,5 +21,7 @@ int main(){
     printf("Max: %d\n", MAX);
     printf("Tong a + b = %d\n", SUM(5, 4));
     printf("Min a, b: %d\n", MIN(4, 6));
+
+    test();
     return 0;
 }
