41dad354211e97b48df5e182152dc352b2fe0127
nammesut <hoangnammst777@gmail.com>
2023-05-28 10:17:19 +0700

    Calculate size of struct

--- ON_TAP/Size_of_Struct.c
+++ ON_TAP/Size_of_Struct.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
-union sizeofUnion
+struct sizeofStruct1
 {
-    uint32_t x;
-    uint8_t s[16];
-    uint64_t f;
-}uni;
+    char a;
+    int b;
+    double c;
+}str1;
+
+struct sizeofStruct2
+{
+    uint32_t a[3];
+    uint8_t b[4];
+    uint16_t c[2];
+}str2;
+
+struct sizeofStruct3
+{
+    uint8_t a[3];
+    uint32_t b[2];
+    uint16_t c[2];
+}str3;
 
 int main()
 {
-    printf("%lu \n", sizeof(uni));
+    printf("%lu \n", sizeof(str3));  
 }
