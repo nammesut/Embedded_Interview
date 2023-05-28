ba59b43c22bac1f8afcb2428655fb9d88720e190
nammesut <hoangnammst777@gmail.com>
2023-05-28 11:53:04 +0700

    Declare pointer

--- ON_TAP/Pointer.c
+++ ON_TAP/Pointer.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    char *a[] = {"Hello", "234"};
+void tong(int a, int b){
+    printf("%d\n", a+b);
+}
 
-    printf("%s\n", );
-    
+void tinhtoan(int a, int b, void (*ptr)(int, int)){
+    ptr(a, b);
+}
+
+int main()
+{
+    /*Con trỏ hàm*/
+    void (*ptr)(int, int) = NULL; 
+    ptr = &tong;
+    ptr(5, 6);
+
+    tinhtoan(7, 10, tong);
+
+    /*Khai báo con trỏ void*/
+    int a = 10;
+    double b = 1.2;
+
+    void *ptr_void = &a;
+    printf("%d\n", *(int *)ptr);
+
+    ptr_void = &b;
+    printf("%f\n", *(double *)ptr_void);
+
+    ptr_void = &tong;
+    ((void (*)(int, int))ptr_void)(12, 10);
 }
