06103ba14c43a550c0cf4e82c608e7adfa879ccb
nammesut <hoangnammst777@gmail.com>
2023-06-07 21:22:44 +0700

    Tinh gia cuoc taxi

--- BAI_TAP/Tinh_cuoc_Taxi/Source/TinhCuocTaxi.c
+++ BAI_TAP/Tinh_cuoc_Taxi/Source/TinhCuocTaxi.c
@@ -1,14 +1,20 @@
+/** Tinh gia cuoc taxi:
+ * Neu di <= 1km: 14000
+ * Neu di <= 30km: 10000
+ * Neu di > 30km: 8000
+ */
+
 #include "head.h"
 
-int tinhCuocTaxi(int n){
-    if(n <= 1){
+int tinhCuocTaxi(int soKm){
+    if(soKm <= 1){
         return 14000;
     }
-    else if (n <= 30){
-        return 14000 + (n-1)*10000;
+    else if (soKm <= 30){
+        return 14000 + (soKm-1)*10000;
     }
     else{
-        return 14000 + 29*10000 + (n-30)*8000;
+        return 14000 + 29*10000 + (soKm-30)*8000;
     }
     
 }
