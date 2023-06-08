b4b4c4f6dfc3a0b82ab44ba1ee4b4ba0c98cfde5
nammesut <hoangnammst777@gmail.com>
2023-06-08 11:23:54 +0700

    Nhap vao bieu thuc va tinh gia tri

---
+++ BAI_TAP/Tinh_bieu_thuc_nhap_vao/Source/TinhBieuThucNhapVao.c
@@ -0,0 +1,76 @@
+/** Tinh bieu thuc nhap vao la chuoi:
+ * 2 + 3 - (6*8) = ?
+ * PT bac 1: 2x + 5 = 0 => x = ?
+ * PT bac 2: 2x^2 + 4x - 6 = 0 => x1, x2 = ?  
+ */
+#include <stdio.h>
+#include <string.h>
+#include "head.h"
+
+int addsub();
+int muldiv();
+int term();
+
+char arr[100];
+int i = 0;
+
+int term(){
+  int n = 0;
+
+  if(arr[i] == '('){
+    i++;
+    n = addsub();
+    
+    if(arr[i] == ')'){
+      i++;
+      return n;
+    }
+  }
+  else{
+    while('0' <= arr[i] && arr[i] <= '9'){
+      n = (arr[i] - '0');
+      i++;
+    }
+  }
+  return n;
+}
+
+int muldiv(){
+  int a,b;
+  
+  a = term();
+  for(;;){
+    if(arr[i] == '*'){
+      i++;
+      b = term();
+      a *= b; 
+    }else if(arr[i] == '/'){
+      i++;
+      b = term();
+      a /= b;
+    }else{
+      return a;
+    }
+  }
+}
+
+int addsub(){
+  int a,b;
+  
+  a = muldiv();
+  
+  for(;;){
+    if(arr[i] == '+'){
+      i++;
+      b = muldiv();
+      a += b; 
+    }else if(arr[i] == '-'){
+      i++;
+      b = muldiv();
+      a -= b;
+    }else{
+      return a;
+    }
+  }
+  
+}
