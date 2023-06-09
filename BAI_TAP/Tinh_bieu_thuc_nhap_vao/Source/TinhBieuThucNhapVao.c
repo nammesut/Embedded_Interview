9d4c1e2f895f2d1f1a19f3050d24ae2d5aa797a3
nammesut <hoangnammst777@gmail.com>
2023-06-09 08:11:59 +0700

    Nhap bieu thuc va tinh gia tri

--- BAI_TAP/Tinh_bieu_thuc_nhap_vao/Source/TinhBieuThucNhapVao.c
+++ BAI_TAP/Tinh_bieu_thuc_nhap_vao/Source/TinhBieuThucNhapVao.c
@@ -1,7 +1,7 @@
 /** Tinh bieu thuc nhap vao la chuoi:
- * 2 + 3 - (6*8) = ?
- * PT bac 1: 2x + 5 = 0 => x = ?
- * PT bac 2: 2x^2 + 4x - 6 = 0 => x1, x2 = ?  
+ * 2+3-(6*8) = ?
+ * PT bac 1: 2x+5=0 => x = ?
+ * PT bac 2: 2x^2+4x-6 = 0 => x1, x2 = ?  
  */
 #include <stdio.h>
 #include <string.h>
@@ -65,6 +65,33 @@ int addsub(){
       b = muldiv();
       a -= b;
     }else{
+      return a;
+    }
+  }
+  
+}
+
+int ptBacI(){
+  int a,b;
+  
+  a = addsub();
+  
+  for(;;){
+    if(arr[i] == 'x'){
+      i++;
+      if(arr[i] == '+'){
+        i++;
+        b = addsub();
+        a = -(b/a); 
+      }
+      else if (arr[i] == '-'){
+        i++;
+        b = addsub();
+        a = b/a; 
+      } 
+      printf("Bieu thuc nhap vao la PT bac 1: x = %d\n", a);
+    }else{
+      printf("%d\n", a);
       return a;
     }
   }
